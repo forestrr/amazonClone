29bfeaf613886cd257466df9b094326103b82f0f
forestrr <afivmd78@gmail.com>
2022-01-07 22:13:08 +0400

    design of the first page upto product fetch is complete max width and coloum still left

--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -1,7 +1,21 @@
-function ProductFeed() {
+import Product from "./Product"
+function ProductFeed({products}) {
+    
+    
     return (
         <div>
             <p>products</p>
+            {products.map(({id , title ,price ,image  ,description,category})=>(
+            <Product
+                key={id}
+                title={title}
+                price={price}
+                image={image}
+                description={description}
+                category={category}
+                
+            />
+            ))}
             
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Banner from '../components/Banner'
 import Header from '../components/Header'
 import ProductFeed from '../components/ProductFeed'
 
-export default function Home() {
+export default function Home(props) {
+  const {products} =props
   return (
     <div className="bg-gray-100 ">
       <Head>
@@ -12,14 +13,26 @@ export default function Home() {
       </Head>
       {/* Header added below from the Header component */}
       <Header/>
-      <main className='max-w-screen-xl mx-auto'>
+      <main className='max-w-screen-2xl mx-auto'>
         {/* Banner added below from the banner component */}
           <Banner/>
         {/* ProductFeed  from  the Product feed components */}
-          <ProductFeed/>
+          <ProductFeed products={products} />
+        
       </main>
 
       
     </div>
   )
 }
+export async function getServerSideProps(context){
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    res=>res.json())
+    return{
+      props:
+      {
+        products,
+      }
+    }
+}
+//https://fakestoreapi.com/products
