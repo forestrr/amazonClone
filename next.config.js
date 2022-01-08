3571b57ccdbc7451c9b525f98cacc2ccec2a5bc5
forestrr <afivmd78@gmail.com>
2022-01-08 11:44:46 +0400

    the data of image endpoint api has been fixed

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports={
     images:{
-        domains:["links.papareact.com","fakestoreapi.com,www.gfinityplc.com"]
+        domains:["links.papareact.com","fakestoreapi.com","www.gfinityplc.com"]
     }
 }
